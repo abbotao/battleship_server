621c14ee1689e9e2c4436fd518d9c2e5c62a4087
Andrew Abbott <andrew.abbott@dynagility.com>
2013-02-22 11:13:56 -0500

    Adding CORS header.

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import render_template
 from flask import jsonify
 from flask import abort
 from flask import request
+from flask import make_response
 
 import os
 import time
@@ -14,6 +15,11 @@ app = Flask(__name__)
 
 games = {}
 
+
+def inject_cors(data):
+    response = make_response(data)
+    response.headers['Access-Control-Allow-Origin'] = "*"
+    return response
 
 @app.route("/")
 def index():
@@ -39,7 +45,7 @@ def new_game():
     if not newgame.place_player_ships(player_ships):
         abort(400)
 
-    return jsonify(id=unicode(newgame.id), ships=dict([(SHIP_NAMES[ship[0]], list(ship[1])) for ship in newgame.player_ships]))
+    return inject_cors(jsonify(id=unicode(newgame.id), ships=dict([(SHIP_NAMES[ship[0]], list(ship[1])) for ship in newgame.player_ships])))
 
 
 @app.route("/game/<id>", methods=["POST"])
@@ -56,9 +62,9 @@ def post_turn(id):
     response_shot = game.make_shot()
     game_over = game.check_game_over()
 
-    return jsonify(hit=shot_result[0], sunk=shot_result[1],
+    return inject_cors(jsonify(hit=shot_result[0], sunk=shot_result[1],
         ship=shot_result[2], shot=response_shot, game_over=game_over[0],
-        winner=game_over[1])
+        winner=game_over[1]))
 
 
 @app.route("/game/<id>", methods=["GET"])
@@ -68,8 +74,10 @@ def get_status(id):
 
     game = games[id]
 
-    return jsonify(ships=dict([(SHIP_NAMES[ship[0]], list(ship[1])) for ship in game.player_ships]))
+    return inject_cors(jsonify(ships=dict([(SHIP_NAMES[ship[0]], list(ship[1])) for ship in game.player_ships])))
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
+
